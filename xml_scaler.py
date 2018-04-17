82b390ff86245240b89a5a9984176f444251c665
M-Agi <agnes.macsotai@gmail.com>
2018-04-17 15:30:23 +0200

    Create xml writer function

--- xml_scaler.py
+++ xml_scaler.py
@@ -6,23 +6,23 @@ import re
 pattern = "\d+"
 SCALE = 4
 
+
 def all_scaler(node):
-	if re.match(pattern, node.text)
-		node.text = str(int(node.text) * SCALE)
-	elif len(list(node)) != 0:
-		for kid in node:
-			all_scaler(kid)
+    if re.match(pattern, node.text):
+        node.text = str(int(node.text) * SCALE)
+    elif len(list(node)) != 0:
+        for kid in node:
+            all_scaler(kid)
 
 
 def parse_xml(filename):
-    #pass
     tree = ET.parse(filename)
     root = tree.getroot()
     for child in root:
         if child.tag == "DefaultSpriteProperties":
             for item in child:
                 if re.match(pattern, item.text):
-                    item.text = item.text * SCALE
+                    item.text = str(int(item.text) * SCALE)
         elif child.tag in ("SpriteTable", "AnimationTable", "NinePatchTable"):
             for item in child:
                 if item.tag == "Overwrite":
@@ -30,7 +30,9 @@ def parse_xml(filename):
     return tree
 
 def write_xml(tree, output):
-    pass
+    with open(output, "w") as xmlFile:
+        xmlFile.write('<?xml version="1.0" encoding="utf-8"?>\n')
+        tree.write(xmlFile, encoding='utf-8')
 
 if len(sys.argv) != 2:
     print "Not enough argument added"
@@ -39,8 +41,7 @@ else:
     if not os.path.isfile(inputFilename):
         print "The added parameter is a directory give a file"
     else:
-        outputFilename = inputFilename.result
+        outputFilename = inputFilename + ".result"
         parsed_tree = parse_xml(inputFilename)
         write_xml(parsed_tree, outputFilename)
-
 
