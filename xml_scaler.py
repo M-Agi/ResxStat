19bbb36b4b04be92070eb42407a0af42f05a608d
M-Agi <agnes.macsotai@gmail.com>
2018-04-27 15:27:02 +0200

    Fix review findings

--- xml_scaler.py
+++ xml_scaler.py
@@ -3,38 +3,50 @@ import xml.etree.ElementTree as ET
 import sys
 import re
 
-pattern = "\d+"
+pattern = "-?\d+"
 SCALE = 4
 
 
-def all_scaler(node):
+def scale_node_text(node):
+    node.text = str(int(node.text) * SCALE)
+
+
+def scaler(node):
     if re.match(pattern, node.text):
-        node.text = str(int(node.text) * SCALE)
+        scale_node_text(node)
     elif len(list(node)) != 0:
         for kid in node:
-            all_scaler(kid)
+            scaler(kid)
 
 
-def parse_xml(filename):
+def process_property(node):
+    for item in node:
+        if re.match(pattern, item.text):
+            scale_node_text(item)
+
+
+def process_sprites(node):
+    for item in node.findall("Entry"):
+        for kid in item.findall("Overwrite"):
+            scaler(kid)
+
+
+def process_xml(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     for child in root:
         if child.tag == "DefaultSpriteProperties":
-            for item in child:
-                if re.match(pattern, item.text):
-                    item.text = str(int(item.text) * SCALE)
+            process_property(child)
         elif child.tag in ("SpriteTable", "AnimationTable", "NinePatchTable"):
-            for item in child:
-                if item.tag == "Entry":
-                    for kid in item:
-                        if str(kid.tag) == "Overwrite":
-                            all_scaler(kid)
+            process_sprites(child)
     return tree
+
 
 def write_xml(tree, output):
     with open(output, "w") as xmlFile:
         xmlFile.write('<?xml version="1.0" encoding="utf-8"?>\n')
         tree.write(xmlFile, encoding='utf-8')
+
 
 if len(sys.argv) != 2:
     print "Not enough argument added"
@@ -44,6 +56,6 @@ else:
         print "The added parameter is a directory give a file"
     else:
         outputFilename = inputFilename + ".result"
-        parsed_tree = parse_xml(inputFilename)
+        parsed_tree = process_xml(inputFilename)
         write_xml(parsed_tree, outputFilename)
 
